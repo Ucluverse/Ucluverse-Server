3206fb879b622b11bcaa41414a935120870533ac
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-03-10 03:52:01 +0900

    init: init project

---
+++ src/test/java/com/ucluverse/newucluverseserver/NewUcluverseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucluverse.newucluverseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewUcluverseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
