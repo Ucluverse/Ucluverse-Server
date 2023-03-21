a9331c4a213efe94914245a9fc64cda37f1b6624
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-03-21 23:45:57 +0900

    Feat: 시큐리티 config login request에 대해서 허용

--- src/main/java/com/ucluverse/newucluverseserver/config/SecurityConfiguration.java
+++ src/main/java/com/ucluverse/newucluverseserver/config/SecurityConfiguration.java
@@ -21,90 +21,10 @@ public class SecurityConfiguration {
                 .httpBasic().disable()
                 .csrf().disable()
                 .cors().and()
-                .authorizeHttpRequests().anyRequest().authenticated()
+                .authorizeHttpRequests().requestMatchers("/members/login").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 }
-
-/*
-@Configuration
-@EnableWebSecurity
-public class AuthorizeUrlsSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests()
-                .requestMatchers("/**").hasRole("USER")
-                .and()
-                .formLogin();
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN", "USER")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-}
-
-    We can also configure multiple URLs. The configuration below requires authentication to every URL and will grant access to URLs starting with /admin/ to only the "admin" user. All other URLs either user can access.
-@Configuration
-@EnableWebSecurity
-public class AuthorizeUrlsSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests()
-                .requestMatchers("/admin").hasRole("ADMIN")
-                .requestMatchers("/**").hasRole("USER")
-                .and()
-                .formLogin();
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN", "USER")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-}
-
-    Note that the matchers are considered in order. Therefore, the following is invalid because the first matcher matches every request and will never get to the second mapping:
-@Configuration
-@EnableWebSecurity
-public class AuthorizeUrlsSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests()
-                .requestMatchers("/**").hasRole("USER")
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .and()
-                .formLogin();
-        return http.build();
-    }
-}
-*/
