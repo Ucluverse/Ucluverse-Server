e907ee7c93019ef90ac96b709eb4fdd31f1aaeea
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-03-22 23:51:02 +0900

    Feat: Member 클래스가 UserDetails implement 하게 구현

--- src/main/java/com/ucluverse/newucluverseserver/domain/member/Member.java
+++ src/main/java/com/ucluverse/newucluverseserver/domain/member/Member.java
@@ -1,12 +1,24 @@
 package com.ucluverse.newucluverseserver.domain.member;
 
+import com.ucluverse.newucluverseserver.common.BaseEntity;
 import com.ucluverse.newucluverseserver.domain.department.Department;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Entity
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
-public class Member {
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Member extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,10 +27,48 @@ public class Member {
     private String nickname;
     private String contactNumber;
     private String email;
-    private Role role;
+    @ElementCollection()
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id")
     private Department department;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return "1111";
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
