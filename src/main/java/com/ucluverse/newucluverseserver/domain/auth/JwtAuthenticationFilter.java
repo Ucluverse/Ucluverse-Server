4616a2d414e8978992439e65c775556440c089b7
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-03-23 02:57:33 +0900

    Feat: JwtAuthenticationFilter 구현

--- src/main/java/com/ucluverse/newucluverseserver/domain/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/ucluverse/newucluverseserver/domain/auth/JwtAuthenticationFilter.java
@@ -17,7 +17,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = tokenProvider.resolveToken(request);
-
         // 토큰이 유효시
         if (token != null && tokenProvider.validateToken(token)) {
             // 토큰으로부터 유저 정보를 받아
