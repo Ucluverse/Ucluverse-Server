0b099372d20e9c88f98786d240066d661c7a5074
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-03-22 23:50:22 +0900

    Feat: TokenProvider 구현

--- src/main/java/com/ucluverse/newucluverseserver/domain/auth/TokenProvider.java
+++ src/main/java/com/ucluverse/newucluverseserver/domain/auth/TokenProvider.java
@@ -1,71 +1,81 @@
 package com.ucluverse.newucluverseserver.domain.auth;
 
+import com.nimbusds.openid.connect.sdk.assurance.evidences.attachment.HashAlgorithm;
+import com.ucluverse.newucluverseserver.domain.member.Role;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
-import lombok.AllArgsConstructor;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.List;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TokenProvider {
 
     @Value("${jwt.secret}")
     private String jwtSecret;
     @Value("${jwt.token-validity-in-seconds}")
-    private int jwtExpirationInMs;
-    private final Key key;
+    private int tokenValidTime = 600;
+    private Key key;
+    private final CustomUserDetailsService userDetailsService;
 
-    public TokenProvider() {
+
+    @PostConstruct
+    protected void init(){
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateToken(Authentication authentication) {
-        Date currentDate = new Date();
-        Date expireDate = new Date(currentDate.getTime() + jwtExpirationInMs);
-
+    public String generateToken(String email, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(String.valueOf(email));
+        claims.put("roles", roles);
+        Date now = new Date();
+        System.out.println(Jwts.builder()
+                .signWith(key)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
+                .compact());
         return Jwts.builder()
-                .setSubject(authentication.getName())
-                .setIssuedAt(new Date())
-                .setExpiration(expireDate)
-                .signWith(key, SignatureAlgorithm.HS512)
+                .signWith(key)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
                 .compact();
     }
 
-    // get username from the token
-    public String getUsernameFromJWT(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-
-        return claims.getSubject();
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getMemberId(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public boolean validateToken(String token) {
+    public String getMemberId(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String jwtToken) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
-            return true;
-        } catch (SignatureException ex){
-            throw new APIException("Invalid JWT signature", HttpStatus.BAD_REQUEST);
-        } catch (MalformedJwtException ex) {
-            throw new APIException("Invalid JWT token", HttpStatus.BAD_REQUEST);
-        } catch (ExpiredJwtException ex) {
-            throw new APIException("Expired JWT token", HttpStatus.BAD_REQUEST);
-        } catch (UnsupportedJwtException ex) {
-            throw new APIException("Unsupported JWT token", HttpStatus.BAD_REQUEST);
-        } catch (IllegalArgumentException ex) {
-            throw new APIException("JWT claims string is empty.", HttpStatus.BAD_REQUEST);
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
         }
     }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authentication");
+    }
+
 }
