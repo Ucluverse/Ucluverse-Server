42e1e0ecee6e8ccc38be3e325681c8b50a646696
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-04-15 23:57:53 +0900

    Refactor: exception 처리 responseStatusException으로 처리

--- src/main/java/com/ucluverse/newucluverseserver/domain/member/MemberService.java
+++ src/main/java/com/ucluverse/newucluverseserver/domain/member/MemberService.java
@@ -5,7 +5,9 @@ import com.ucluverse.newucluverseserver.domain.department.DepartmentRepository;
 import com.ucluverse.newucluverseserver.domain.member.dto.MemberLoginRequest;
 import com.ucluverse.newucluverseserver.domain.member.dto.MemberSignUpRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,11 +21,11 @@ public class MemberService {
 
     public Member signUp(MemberSignUpRequest dto ){
         memberRepository.findOneByEmail(dto.getEmail()).ifPresent(a -> {
-            throw new IllegalArgumentException("이미 가입된 email 입니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다.");
         });
         List<String> roles = new ArrayList<>();
         roles.add(Role.USER.getKey());
-        Department department = departmentRepository.findOneByName(dto.getDepartment()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 학과입니다."));
+        Department department = departmentRepository.findOneByName(dto.getDepartment()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "학과 정보가 존재하지 않습니다."));
         Member member = Member.builder()
                 .email(dto.getEmail())
                 .userName(dto.getUserName())
@@ -37,9 +39,9 @@ public class MemberService {
 
     public String login(MemberLoginRequest dto ){
         if(!dto.getEmail().split("@")[1].equals("ajou.ac.kr")){
-            throw new IllegalArgumentException("아주대 이메일이 아닙니다");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "아주대 이메일이 아닙니다.");
         }
-        Member member = memberRepository.findOneByEmail(dto.getEmail()).orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        Member member = memberRepository.findOneByEmail(dto.getEmail()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "가입되지 않은 이메일입니다."));
         return tokenProvider.generateToken(member.getEmail(), member.getRoles());
     }
 
